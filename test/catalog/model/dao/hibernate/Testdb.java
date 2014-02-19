ef12c7414b8e4b615f3f291646c5536378daeff6
RLDevOps <sandeep.ranganath@relevancelab.com>
2014-02-19 12:52:18 +0530

    Update Testdb.java

--- test/catalog/model/dao/hibernate/Testdb.java
+++ test/catalog/model/dao/hibernate/Testdb.java
@@ -37,7 +37,7 @@ public class Testdb extends DatabaseTestCase {
 
 		   Connection jdbcConnection = 
 			 (Connection) DriverManager.getConnection(
-		  "jdbc:mysql://54-84-108-72:3306/catalog", "root", "admin");
+		  "jdbc:mysql://54.84.108.72:3306/catalog", "root", "admin");
 		   
 		   ret DatabaseConnection(jdbcConnection);
 	}
